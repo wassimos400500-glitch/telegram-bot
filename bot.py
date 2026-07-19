e4d7d55005b3ab31d2e67f66fbe5d503c761b1ac
wassimos400500-glitch <wassimos400500@gmail.com>
2026-07-19 23:47:49 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+import os
+
+TOKEN = os.getenv"8766911595:AAH1u67LIcIFwbal5wznLXxxEso21Mbak0E"
+
+responses = {
+    "واه": "متلعبهاش وهراني يا واحد القسنطيني",
+    "مهم": "صايي روح برب نبقاو نهدروا برك",
+    "علاه": "وش دخلك",
+    "وسيم": "my father"
+}
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.strip()
+
+    if text in responses:
+        await update.message.reply_text(responses[text])
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT, reply))
+
+print("Bot started...")
+app.run_polling()
